c8184504c7dd7efa90b6f68c9ff87142347afb20
Peter-English <ptrdeathonline@gmail.com>
2020-06-22 13:20:04 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+const App = () => {
+  return <h1 className="container">App works!</h1>;
+};
+
+export default App;
